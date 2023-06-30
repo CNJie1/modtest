f5cf530d8c86d513206c0fa084f64df77598cc55
print <110807969+CNJie1@users.noreply.github.com>
2023-06-30 10:33:59 +0800

    Update Customization.js

--- Customization.js
+++ Customization.js
@@ -1,17 +1,28 @@
- function executePatchx1() {
-	          var inputNum = document.getElementById("inputNum").value;
-	          var modules = h5gg.getRangesList("UnityFramework");
-	          var base = modules[0].start;
-	          var addr = Number(base) + 0x3BD2ABC;
-	          var hex = inputNum.toString(16).toUpperCase(); // 将输入的整数转换为十六进制，并转换为大写形式
-	  
-	          // 将用户输入的十六进制值传递给 patchBytes() 函数
-	          patchBytes(addr, hex);
-	      }
-	  
-	      function patchBytes(addr, hex) {
-	          for (var i = 0; i < hex.length / 2; i++) {
-	              var item = parseInt(hex.substring(i * 2, i * 2 + 2), 16);
-	              h5gg.setValue(addr + i, item, "U8");
-	          }
-	      }
+function convertAndPatch() {
+	var modules = h5gg.getRangesList("UnityFramework"); //获取模块列表
+	var base = modules[0].start; //获取第一个模块的基地址
+  var number = document.getElementById("inputNumber").value; // 获取输入的数字
+  var hexString = "~A8 CMP W0, #" + number; // 将数字拼接为ARM指令字符串
+
+  // 调用patchBytes函数执行ARM指令
+  patchBytes(hexString);
+}
+
+function patchBytes(armInstruction) {
+  var addr = Number(base) + 0x3BD2ABC; // 计算要修改的地址
+  var hexValue = armInstructionToHex(armInstruction); // 将ARM指令转换为十六进制值
+
+  for (var i = 0; i < hexValue.length / 2; i++) {
+    var item = parseInt(hexValue.substring(i * 2, i * 2 + 2), 16);
+    h5gg.setValue(addr + i, item, "U8"); // 调用h5gg函数修改内存值
+  }
+}
+
+function armInstructionToHex(armInstruction) {
+  var hex = "";
+  for (var i = 0; i < armInstruction.length; i++) {
+    var charCode = armInstruction.charCodeAt(i);
+    hex += charCode.toString(16).toUpperCase();
+  }
+  return hex;
+}

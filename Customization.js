24b889918437a1e7ee77d76f6104dd0493a12881
print <110807969+CNJie1@users.noreply.github.com>
2023-07-13 02:16:24 +0800

    Update Customization.js

--- Customization.js
+++ Customization.js
@@ -1,10 +1,17 @@
-function patchBytes() {
-	var modules = h5gg.getRangesList("UnityFramework");
-	var base = modules[0].start;
-	var addr = Number(base) + 0x3BBBCC4;
-
-  var inputField = document.getElementById("inputNum");
-  var userInput = inputField.value;
-  h5gg.setValue(addr, userInput, "U8"); // 写入内存地址
-  inputField.value = ""; // 清空输入字段
+function Customization(){
+	var inputField = document.getElementById("inputNum");
 }
+var modules = h5gg.getRangesList("UnityFramework"); //库
+  
+  var base = modules[0].start;//运行时内存中的模块基地址
+  
+  var addr = Number(base) + 0x2DB2B78; //偏移量
+  
+  patchBytes(addr,inputField); //字节
+  //只有越狱设备才能做到这一点
+  function patchBytes(addr, hex) {
+      for(i = 0;i<hex.length/2;i++) {
+          var item = parseInt(hex.substring(i*2, i*2+2), 16);
+          h5gg.setValue(addr+i,item, "U8");
+      }
+   }
